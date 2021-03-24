5c47251c2aeb0c8e7a8d571833253376c06cc0a9
Almedin Alibasic <alibasicalmedin@gmail.com>
2021-03-24 02:07:26 +0100

    commited

---
+++ weather_app/scripts/forecast.js
@@ -0,0 +1,29 @@
+const key = '0SBGvP0lcE3Kn5VzWUIQeYWaaH7T9QND';
+
+// get weather information
+const getWeather = async (id) => {
+  
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+  
+    const response = await fetch(base + query);
+    const data = await response.json();
+  
+    return data[0];
+  
+  };
+  
+  // get city information
+  const getCity = async (city) => {
+  
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+  
+    const response = await fetch(base + query);
+    const data = await response.json();
+  
+    return data[0];
+  
+  };
+
+
